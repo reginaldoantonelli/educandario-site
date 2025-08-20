bcff2f55229448b024ad52b550e74bcbf24a5798
reginaldoantonelli <antonellireginaldo@hotmail.com>
2025-08-20 10:45:17 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Carregar header
+  fetch("header.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("header").innerHTML = data;
+    });
+
+  // Carregar footer
+  fetch("footer.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("footer").innerHTML = data;
+    });
+});
