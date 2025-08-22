3d54a659f68a6105e043b36dd9dbeb9e7bb2982a
reginaldoantonelli <antonellireginaldo@hotmail.com>
2025-08-22 14:12:40 -0300

    botao doacao pop up index

--- script.js
+++ script.js
@@ -12,34 +12,5 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(data => {
       document.getElementById("footer").innerHTML = data;
     });
-});
-let index = 0;
-
-function showSlide(i) {
-  const slides = document.querySelectorAll(".slide");
-  if (i >= slides.length) index = 0;
-  if (i < 0) index = slides.length - 1;
-
-  slides.forEach((slide, idx) => {
-    slide.style.transform = `translateX(${100 * (idx - index)}%)`;
-  });
-}
-
-function nextSlide() {
-  index++;
-  showSlide(index);
-}
-
-function prevSlide() {
-  index--;
-  showSlide(index);
-}
-
-// inicia carrossel
-document.addEventListener("DOMContentLoaded", () => {
-  showSlide(index);
-  setInterval(nextSlide, 5000); // troca a cada 5s
-
-  document.querySelector(".next").addEventListener("click", nextSlide);
-  document.querySelector(".prev").addEventListener("click", prevSlide);
+    
 });
